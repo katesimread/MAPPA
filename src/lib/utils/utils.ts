86739b997bc5440591d98a078c539d1cfab2ee63
katesimread <katesimread0@gmail.com>
2026-06-27 16:41:46 +0100

    refactor: remove TypeScript source files replaced by JavaScript

--- src/lib/utils/utils.ts
+++
@@ -1,8 +0,0 @@
-export const roundCoordinates = (
-  coordinates: [number, number],
-  decimal_points = 6
-): [number, number] => {
-  return coordinates.map((coord) =>
-    parseFloat(coord.toFixed(decimal_points))
-  ) as [number, number];
-};
